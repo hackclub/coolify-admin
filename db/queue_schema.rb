54f8754d7a470da2c46c8be56b622a2fc38c0c91
Zach Latta <zach@zachlatta.com>
2025-10-16 20:40:03 +0000

    Add script to sync prod data to dev

--- db/queue_schema.rb
+++ db/queue_schema.rb
@@ -141,3 +141,4 @@ ActiveRecord::Schema[8.0].define(version: 1) do
   add_foreign_key "solid_queue_ready_executions", "solid_queue_jobs", column: "job_id", on_delete: :cascade
   add_foreign_key "solid_queue_recurring_executions", "solid_queue_jobs", column: "job_id", on_delete: :cascade
   add_foreign_key "solid_queue_scheduled_executions", "solid_queue_jobs", column: "job_id", on_delete: :cascade
+end
